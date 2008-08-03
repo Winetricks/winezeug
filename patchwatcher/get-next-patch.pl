e12f8af328550368a8860dcc53d6b40639591708
daniel.r.kegel <daniel.r.kegel@065a81bf-8143-0410-bb62-c3a588fc7d44>
2008-08-03 10:33:40 +0000

    Get ready to handle patch series
    Fix missing 'test' in 'if -d'
    
    For now, don't actually delete messages from inbox, still debugging

--- patchwatcher/get-next-patch.pl
+++ patchwatcher/get-next-patch.pl
@@ -3,8 +3,7 @@
 # Also delete any non-patch email encountered
 # Dan Kegel 2008
 
-# TODO: recognize patch series by the convention %d/%d in the subject
-# line and only retrieve them in order when the patch series is complete
+# TODO: sort patch series and output them when complete in proper order
 
 use strict;
 use warnings;
@@ -17,51 +16,82 @@ my $pop = new Mail::POP3Client(
                  HOST     => $ENV{"PATCHWATCHER_HOST"}
 );
 
-## for HeadAndBodyToFile() to use
-my $fh = new IO::Handle();
-
 ## Initialize stuff for MIME::Parser;
 # TODO: stop using outputdir
 my $outputdir = "./mimemail";
 my $parser = new MIME::Parser;
 $parser->output_dir($outputdir);
 
+# Read in all patches, assign them to patch groups,
+# output all complete patch groups.
+# Argument is number of first patch to output.
+# Patches are output as %d.patch.
+
+my $curpatch = $ARGV[0];
+if ($curpatch eq "") {
+    print "Usage: perl get-patches.pl starting-patch-number\n";
+    exit(1);
+}
+
+my $patches_written = 0;
+
+sub output_patch
+{
+    my $header = $_[0];
+    my $body = $_[1];
+    open FILE, "> $curpatch.patch" || die "can't create $curpatch.patch";
+    $curpatch++;
+    $patches_written++;
+
+    print FILE "From: ".$header->get('From');
+    print FILE "Subject: ".$header->get('Subject');
+    print FILE "Date: ".$header->get('Date');
+    print FILE "\n";
+    print FILE $body;
+
+    close FILE;
+}
+
 my $i;
-my $done = 0;
-## get first patch in pop3 inbox
-for ($i = 1; $i <= $pop->Count() && !$done; $i++) {
+for ($i = 1; $i <= $pop->Count(); $i++) {
    my $msg;
    $msg = $pop->HeadAndBody( $i );
-   $pop->Delete( $i );
    my $entity = $parser->parse_data($msg);
 
    $entity->make_singlepart;
 
    if ($entity->parts < 2) {
-	if ($entity->bodyhandle->as_string =~ m/diff/) {
-		print "From: ".$entity->head->get('From');
-		print "Subject: ".$entity->head->get('Subject');
-		print "Date: ".$entity->head->get('Date');
-		print "\n";
-		print $entity->bodyhandle->as_string;
-                $done = 1;                
-	}
+        if ($entity->bodyhandle->as_string =~ m/diff/) {
+            if ($entity->head->get('Subject') !~ /\d\/\d/) {
+                output_patch($entity->head, $entity->bodyhandle->as_string);
+                #$pop->Delete( $i );
+            } else {
+                # part of sequence 
+                # For now, just ignore; we'll deal with them later
+                #$pop->Delete( $i );
+                print "ignoring series". $entity->head->get('Subject')
+            }
+        }
    } else {
-	foreach ($entity->parts) {
-		if ($_->bodyhandle->as_string =~ m/diff/) {
-			print "From: ".$entity->head->get('From');
-			print "Subject: ".$entity->head->get('Subject');
-			print "Date: ".$entity->head->get('Date');
-		        print "\n";
-			print $_->bodyhandle->as_string;
-                        $done = 1;                
-			last;
-		}
-	}
+        foreach ($entity->parts) {
+            if ($_->bodyhandle->as_string =~ m/diff/) {
+                if ($entity->head->get('Subject') !~ /\d\/\d/) {
+                    output_patch($entity->head, $_->bodyhandle->as_string);
+                    #$pop->Delete( $i );
+                } else {
+                    # part of sequence 
+                    # For now, just ignore; we'll deal with them later
+                    #$pop->Delete( $i );
+                    print "ignoring series". $entity->head->get('Subject')
+                }
+                last;
+            }
+        }
    }
 }
 $pop->Close();
-if ($done) {
+
+if ($patches_written > 0) {
     exit(0);
 } else {
     exit(1);
