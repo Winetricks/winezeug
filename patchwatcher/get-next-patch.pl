fc60188a68a545741059112e9962031287daff92
daniel.r.kegel <daniel.r.kegel@065a81bf-8143-0410-bb62-c3a588fc7d44>
2008-08-04 04:34:57 +0000

    Actually handle series halfway right.
    Doesn't handle it well if somebody resends just
    one patch of a series, nor if two people's series
    happen to interleave.  Must be able to handle those,
    but this is a nice stopping point for the day.

--- patchwatcher/get-next-patch.pl
+++ patchwatcher/get-next-patch.pl
@@ -52,6 +52,88 @@ sub output_patch
     close FILE;
 }
 
+my $series_sender = "";
+my $series_num_patches;
+my @series_headers;
+my @series_bodies;
+my @series_indices;
+
+sub consume_series_patch
+{
+    my $header = $_[0];
+    my $body = $_[1];
+    my $index = $_[2];
+    my $which_patch = $_[3];
+    my $num_patches = $_[4];
+
+    my $sender = $header->get('From');
+
+    if ($series_sender eq "") {
+       print "Starting series; sender $sender, num_patches $num_patches, subject ".$header->get('Subject')."\n";
+       $series_sender = $sender;
+       $series_num_patches = $num_patches;
+    }
+
+    if ($series_sender ne $sender) {
+       print "Ignoring series for now, will try later; sender $sender, num_patches $num_patches, subject ".$header->get('Subject')."\n";
+        # can't handle multiple series at once just yet, let it sit
+        return;
+    }
+    print "Saving patch $which_patch\n";
+    $series_headers[$which_patch] = $header;
+    $series_bodies[$which_patch] = $body;
+    $series_indices[$which_patch] = $index;
+
+    # Is the series complete?
+    my $j;
+    for ($j=1; $j <= $series_num_patches; $j++) {
+        last if (! defined($series_indices[$j]));
+    }
+    if ($j == $series_num_patches+1) {
+        # Yes!  Output them all.
+        for ($j=1; $j <= $series_num_patches; $j++) {
+            print "Outputting patch $j of $series_num_patches\n";
+            output_patch($series_headers[$j], $series_bodies[$j]);
+            #$pop->Delete( $series_indices[$j] );
+        }
+        @series_headers = ();
+        @series_bodies = ();
+        @series_indices = ();
+        $series_sender = "";
+        $series_num_patches = "";
+    }
+}
+
+# Return true if patch detected
+# Delete message unless it's a patch we can't consume just yet
+sub consume_message
+{
+    my $header = $_[0];
+    my $body = $_[1];
+    my $index = $_[2];
+
+    if ($body =~ m/diff/) {
+        if ($header->get('Subject') !~ /(\d+)\/(\d+)/) {
+            output_patch($header, $body);
+            #$pop->Delete( $index );
+        } else {
+            # part of sequence 
+            my $which_patch = $1;
+            my $num_patches = $2;
+            if ($which_patch == 0) {
+                # Zeroth patch in series is supposed to be just explanation?
+                #$pop->Delete( $index );
+            } else {
+                consume_series_patch($header, $body, $index, $which_patch, $num_patches);
+            }
+        }
+        return 1;
+    } else {
+        #$pop->Delete( $index );
+    }
+    return 0;
+}
+
 my $i;
 for ($i = 1; $i <= $pop->Count(); $i++) {
    my $msg;
@@ -61,29 +143,10 @@ for ($i = 1; $i <= $pop->Count(); $i++) {
    $entity->make_singlepart;
 
    if ($entity->parts < 2) {
-        if ($entity->bodyhandle->as_string =~ m/diff/) {
-            if ($entity->head->get('Subject') !~ /\d\/\d/) {
-                output_patch($entity->head, $entity->bodyhandle->as_string);
-                #$pop->Delete( $i );
-            } else {
-                # part of sequence 
-                # For now, just ignore; we'll deal with them later
-                #$pop->Delete( $i );
-                print "ignoring series". $entity->head->get('Subject')
-            }
-        }
+        consume_message($entity->head, $entity->bodyhandle->as_string, $i);
    } else {
         foreach ($entity->parts) {
-            if ($_->bodyhandle->as_string =~ m/diff/) {
-                if ($entity->head->get('Subject') !~ /\d\/\d/) {
-                    output_patch($entity->head, $_->bodyhandle->as_string);
-                    #$pop->Delete( $i );
-                } else {
-                    # part of sequence 
-                    # For now, just ignore; we'll deal with them later
-                    #$pop->Delete( $i );
-                    print "ignoring series". $entity->head->get('Subject')
-                }
+            if (consume_message($entity->head, $_->bodyhandle->as_string, $i)) {
                 last;
             }
         }
