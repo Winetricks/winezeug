0f3ad3a4c9d53b955a46c018ca5c1a542ea0a3c3
daniel.r.kegel <daniel.r.kegel@065a81bf-8143-0410-bb62-c3a588fc7d44>
2008-08-29 14:38:48 +0000

    First cut at detecting patches already applied. Based on a patch from Zachary Goldberg

--- trunk/patchwatcher/dashboard.pl
+++ trunk/patchwatcher/dashboard.pl
@@ -25,8 +25,7 @@ print "<title>Wine patch status</title>\n";
 print "<link rel=\"stylesheet\" href=\"winehq.css\" type=\"text/css\">\n";
 print "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n";
 print "</head><body><table>\n";
-print "<col class=\"date\"><col class=\"from\"><col class=\"subject\"><col class=\"result\">
-\n";
+print "<col class=date><col class=from><col class=subject><col class=result>\n";
 print "<tr>";
 print "<th>Date";
 print "<th>From";
@@ -39,20 +38,51 @@ close FILE;
 my $patch;
 for $patch (@patches) {
    chomp($patch);
-   my $log = $patch;
-   $log =~ s/\.txt$/.log/;
-   if (-f $log) {
-       open FILE, "grep . < $log | tail -1 |";
-       $status = <FILE>;
-       chomp($status);
-       close FILE;
-   } else {
+   my $logname = $patch;
+   my $status;
+   my $goodness;
+   my $commitstatus;
+   my $patchresult;
+   my $loglink;
+
+   $logname =~ s/\.txt$/.log/;
+
+   if (! -f $logname) {
        $status = "queued";
+       $goodness = "0";
+   } else {
+       open FILE, $logname;
+       my @log = <FILE>;
+       close FILE;
+       my $log = join('', @log);
+
+       # Parse log and decide if build & test succeeded.
+
+       $status = $log;
+       # Get last nonblank line in file.
+       my $i;
+       for ($i = @log - 1; $log[$i] eq "\n"; $i-- && $i >0) { ; }
+       $status = $log[$i];
+       chomp($status);
+
+       if ($status =~ "Conformance tests ok") {
+           $goodness = 1;
+       } elsif ($log =~ m/can't find file to patch|hunk FAILED/) {
+           $status = "Patch failed.";
+           $goodness = "-1";
+       } elsif ($log =~ m/Reversed \(or previously applied\) patch/){
+           $status = "Patch already in git.";	   
+           $goodness = "1";
+       } else {
+           $goodness = "-1";
+       }
    }
+
    open FILE, $patch;
    binmode FILE, ":utf8";
    my @patch = <FILE>;
    close FILE;
+
    # Grab date, author, and subject from patch
    my @date = grep(/Date:/, @patch);
    my $date = $date[0];
@@ -72,12 +102,12 @@ for $patch (@patches) {
 
    $subject = my_escape($subject);
 
-   if ($status eq "queued") {
-       $loglink = $status;
-   } elsif ($status eq "Conformance tests ok") {
-       $loglink = "<a href=\"$log\" class=\"result, pass\">$status</a>";
+   if ($goodness == -1) {
+       $loglink = "<a href=\"$logname\" class=\"result, fail\">$status</a>";
+   } elsif ($goodness == 1) {
+       $loglink = "<a href=\"$logname\" class=\"result, pass\">$status</a>";
    } else {
-       $loglink = "<a href=\"$log\" class=\"result, fail\">$status</a>";
+       $loglink = $status;
    }
    print "<tr>";
    print "<td class=date>$date &nbsp;";
