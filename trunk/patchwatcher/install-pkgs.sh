7f800b4cef96d26e7ab1852dca12ef7813bbc245
daniel.r.kegel <daniel.r.kegel@065a81bf-8143-0410-bb62-c3a588fc7d44>
2008-10-26 21:37:41 +0000

    Initial documentation Example script showing how to install prerequisites, at least for hardy

---
+++ trunk/patchwatcher/install-pkgs.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Install prerequisites needed to run Patchwatcher
+# Run this as root
+
+# Don't forget normal wine prerequisites!  e.g.
+#  wget http://kegel.com/wine/hardy.sh
+#  sh hardy.sh
+
+# wine-slave.sh
+apt-get install autoconf
+
+# Dashboard2.pl
+apt-get install libdate-manip-perl
+
+# get-patches2.pl
+apt-get install libmail-pop3client-perl libmime-tools-perl
+
+# libpatchwatcher.sh
+apt-get install mailx
