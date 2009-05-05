d129d37b4602c40095576a9a02d5b9f4a81d486b
austinenglish <austinenglish@065a81bf-8143-0410-bb62-c3a588fc7d44>
2009-05-05 06:59:35 +0000

    build_script: be sure to export variables

--- trunk/build_script/daily.sh
+++ trunk/build_script/daily.sh
@@ -66,18 +66,17 @@ set -ex
 # TODO: Find something more portable... `uname -o` fails on FreeBSD, possibly others. Does -s work on Solaris?
 
 OS=`uname -o` || OS=`uname -s`
-echo $OS
 
 # TODO: Differentiate between Solaris and OpenSolaris here...not sure how though :-/
 if [ $OS = 'Solaris' ]
     then
-        CFLAGS="-I/usr/sfw/include -I/usr/X11/include -g" 
-        CPPFLAGS="-I/usr/sfw/include" 
-        CC="/usr/gnu/bin/cc" 
-        LDFLAGS="-L/usr/sfw/lib -R/usr/sfw/lib -L/usr/X11/lib -R/usr/X11/lib" 
-        LD="/usr/gnu/bin/ld" 
-        PATH="/usr/gnu/bin:/usr/bin:/usr/X11/bin:/usr/sbin:/sbin:/opt/csw/bin/:/usr/ccs/bin:/usr/sfw/bin:/usr/local/bin"
-        CONFIGUREFLAGS="--without-ldap --without-capi"
+        export CFLAGS="-I/usr/sfw/include -I/usr/X11/include -g" 
+        export CPPFLAGS="-I/usr/sfw/include" 
+        export CC="/usr/gnu/bin/cc" 
+        export LDFLAGS="-L/usr/sfw/lib -R/usr/sfw/lib -L/usr/X11/lib -R/usr/X11/lib" 
+        export LD="/usr/gnu/bin/ld" 
+        export PATH="/usr/gnu/bin:/usr/bin:/usr/X11/bin:/usr/sbin:/sbin:/opt/csw/bin/:/usr/ccs/bin:/usr/sfw/bin:/usr/local/bin"
+        export CONFIGUREFLAGS="--without-ldap --without-capi"
 elif [ $OS = 'Linux' ] || [ $OS = 'GNU/Linux' ]
     then
 # Unused at the moment, but if there's a distro requiring a strange build setup, this will be useful.
@@ -86,21 +85,21 @@ elif [ $OS = 'Linux' ] || [ $OS = 'GNU/Linux' ]
         echo "If not, please notifiy the maintainer to add your build script here."
 elif [ $OS = 'FreeBSD' ]
     then
-        CC="ccache gcc"
-        CPPFLAGS="-I/usr/local/include"
-        LDFLAGS="-L/usr/local/lib"
+        export CC="ccache gcc"
+        export CPPFLAGS="-I/usr/local/include"
+        export LDFLAGS="-L/usr/local/lib"
 elif [ $OS = 'NetBSD' ]
     then
         echo "This is untested...going from memory"
-        CFLAGS="-O2 -I/usr/pkg/include -I/usr/include -I/usr/pkg/include/freetype2 -I/usr/X11R6/include"
-        CPPFLAGS="-I/usr/pkg/include -I/usr/include -I/usr/pkg/include/freetype2 -I/usr/X11R6/include"
-        LDFLAGS="-L/usr/pkg/lib -Wl,-R/usr/pkg/lib -L/usr/lib -Wl,-R/usr/lib -L/usr/X11R6/lib -Wl,-R/usr/X11R6/lib"
+        export CFLAGS="-O2 -I/usr/pkg/include -I/usr/include -I/usr/pkg/include/freetype2 -I/usr/X11R6/include"
+        export CPPFLAGS="-I/usr/pkg/include -I/usr/include -I/usr/pkg/include/freetype2 -I/usr/X11R6/include"
+        export LDFLAGS="-L/usr/pkg/lib -Wl,-R/usr/pkg/lib -L/usr/lib -Wl,-R/usr/lib -L/usr/X11R6/lib -Wl,-R/usr/X11R6/lib"
 elif [ $OS = 'OpenBSD' ]
     then
-        CFLAGS="-I/usr/local/include -I/usr/local/include/libpng"
-        LDFLAGS="-lm -lz -lcrypto -L/usr/local/lib"
-        X_EXTRA_LIBS="-lXau -lXdmcp"
-        CPPFLAGS="-I/usr/local/include"
+        export CFLAGS="-I/usr/local/include -I/usr/local/include/libpng"
+        export LDFLAGS="-lm -lz -lcrypto -L/usr/local/lib"
+        export X_EXTRA_LIBS="-lXau -lXdmcp"
+        export CPPFLAGS="-I/usr/local/include"
 else
     echo "Your OS is not supported by this build script. Please e-mail the maintainer if you get this message."
     exit 1
