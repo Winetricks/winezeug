78851bf5cddf0d5b2f37900efa98d15ad495aecc
daniel.r.kegel <daniel.r.kegel@065a81bf-8143-0410-bb62-c3a588fc7d44>
2009-10-27 10:24:08 +0000

    Update scripts to what I'm running at the moment

--- valgrind/valgrind-continuous.sh
+++ valgrind/valgrind-continuous.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# Usage: sh tools/valgrind-continuous.sh
+# Usage: sh tools/valgrind/valgrind-continuous.sh
 set -e
 set -x
 while true
 do
-    sh tools/valgrind-sync.sh
-    sh tools/valgrind-daily.sh
-    sh tools/valgrind-upload.sh $1 $2
+    sh tools/valgrind/valgrind-sync.sh
+    sh tools/valgrind/valgrind-daily.sh
+    sh tools/valgrind/valgrind-upload.sh $1 $2
     sleep 10
 done
