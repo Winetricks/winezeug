f65b6aa200971b52347e939d0045e911ae7cd4ec
daniel.r.kegel <daniel.r.kegel@065a81bf-8143-0410-bb62-c3a588fc7d44>
2010-01-18 02:46:42 +0000

    Closer to working on Windows Vista

--- trunk/testsuites/chromium/build.sh
+++ trunk/testsuites/chromium/build.sh
@@ -20,6 +20,9 @@ case "$OS" in
  "Windows_NT") 
    # Windows or Wine
    DRIVE_C=/cygdrive/c
+   PROGRAM_FILES_x86="$DRIVE_C/Program Files (x86)"
+   PROGRAM_FILES="$DRIVE_C/Program Files"
+   WINE="cmd /c"
    ;;
  *)
    # Linux
@@ -28,12 +31,24 @@ case "$OS" in
    WINEPREFIX=${WINEPREFIX:-$HOME/.wine-chromium-tests}
    export WINEPREFIX
    DRIVE_C=$WINEPREFIX/drive_c
+   PROGRAM_FILES_x86="$DRIVE_C/Program Files (x86)"
+   PROGRAM_FILES="$DRIVE_C/Program Files"
+
    echo Using wine=$WINE, with WINEPREFIX=$WINEPREFIX
    if test $do_init = 1
    then
       rm -rf $WINEPREFIX
    fi
+   ;;
 esac
+
+PROGRAM_FILES_x86_WIN="C:\\Program Files (x86)"
+PROGRAM_FILES_WIN="C:\\Program Files"
+if ! test -d "$PROGRAM_FILES_x86"
+then
+   PROGRAM_FILES_x86="$PROGRAM_FILES"
+   PROGRAM_FILES_x86_WIN="$PROGRAM_FILES_WIN"
+fi
 
 while ! test -f "$DRIVE_C/cygwin/bin/unzip.exe"
 do
@@ -43,7 +58,7 @@ done
 
 # http://dev.chromium.org/developers/how-tos/build-instructions-windows#TOC-Prerequisite-software
 # says "install Visual C++ 2005"
-if test ! -f "$DRIVE_C/Program Files/Microsoft Visual Studio 8/VC/vcvarsall.bat"
+if test ! -f "$PROGRAM_FILES_x86/Microsoft Visual Studio 8/VC/vcvarsall.bat"
 then
    time sh ../../winetricks vcrun2005
    time sh ../../winetricks vc2005trial
@@ -54,7 +69,8 @@ fi
 
 # http://dev.chromium.org/developers/how-tos/build-instructions-windows#TOC-Prerequisite-software
 # says "install Windows 7 SDK"
-if test ! -f "$DRIVE_C/Program Files/Microsoft SDKs/Windows/v7.0/Lib/windowscodecs.lib"
+if test ! -f "$PROGRAM_FILES/Microsoft SDKs/Windows/v7.0/Lib/windowscodecs.lib"
+
 then
    echo "Be sure to only select the minimum options needed for C development in the Platform SDK!"
    sh ../../winetricks -v psdkwin7
@@ -69,7 +85,7 @@ fi
 # C:\Program Files\Microsoft Visual Studio 8\VC\vcpackages\VCProjectEngine.dll.config
 # thereafter.
 
-cp VCProjectEngine.dll.config "$DRIVE_C/Program Files/Microsoft Visual Studio 8/VC/vcpackages/"
+cp VCProjectEngine.dll.config "$PROGRAM_FILES_x86/Microsoft Visual Studio 8/VC/vcpackages/"
 
 mkdir -p "$DRIVE_C/chromium"
 cp depot_tools.reg depot_tools.patch "$DRIVE_C/chromium"
@@ -89,19 +105,28 @@ fi
 if test ! -d depot_tools/svn_bin
 then
    # Work around http://bugs.winehq.org/show_bug.cgi?id=19799 and http://bugs.winehq.org/show_bug.cgi?id=21381
-   patch -p0 < depot_tools.patch
+   if test "$OS" != Windows_NT
+   then
+      patch -p0 < depot_tools.patch
+   fi
    # First run causes it to download and install svn.
    $WINE cmd /c gclient help || true
-   # It fails at the very last moment, so do the missing two steps.
-   cp depot_tools/bootstrap/win/python.new.bat depot_tools/python.bat
-   cp depot_tools/bootstrap/win/python.new depot_tools/python
-   # Show that it works.
-   $WINE cmd /c gclient help 
+   if test "$OS" != Windows_NT
+   then
+      # It fails at the very last moment, so do the missing two steps.
+      cp depot_tools/bootstrap/win/python.new.bat depot_tools/python.bat
+      cp depot_tools/bootstrap/win/python.new depot_tools/python
+      # Show that it works.
+      $WINE cmd /c gclient help 
+   fi
 fi
 
-# gyp needs to know username and domain for some reason (see http://code.google.com/p/gyp/issues/detail?id=100 )
-export USERNAME=dank
-export USERDOMAIN=kegel.com
+if test "$OS" != Windows_NT
+then
+   # gyp needs to know username and domain for some reason (see http://code.google.com/p/gyp/issues/detail?id=100 )
+   export USERNAME=dank
+   export USERDOMAIN=kegel.com
+fi
 # Work around Bug 19533 - reg.exe missing most options; breaks firefox and chromium builds
 # http://bugs.winehq.org/show_bug.cgi?id=19533
 export GYP_GENERATORS=msvs
@@ -129,8 +154,8 @@ fi
 
 # Build!  The three projects we want to start with are base_unittests, net_unittests, and unit_tests.
 cd src
-$WINE "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv" /build Debug /out base_unittests.log /project base_unittests chrome\\chrome.sln
-#$WINE "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv" /build Debug /out net_unittests.log /project net_unittests chrome\\chrome.sln
-#$WINE "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv" /build Debug /out unit_tests.log /project unit_tests chrome\\chrome.sln
+$WINE "$PROGRAM_FILES_x86_WIN\\Microsoft Visual Studio 8\\Common7\\IDE\\devenv" /build Debug /out base_unittests.log /project base_unittests chrome\\chrome.sln
+#$WINE "$PROGRAM_FILES_x86_WIN\\Microsoft Visual Studio 8\\Common7\\IDE\\devenv" /build Debug /out net_unittests.log /project net_unittests chrome\\chrome.sln
+#$WINE "$PROGRAM_FILES_x86_WIN\\Microsoft Visual Studio 8\\Common7\\IDE\\devenv" /build Debug /out unit_tests.log /project unit_tests chrome\\chrome.sln
 
 echo done
