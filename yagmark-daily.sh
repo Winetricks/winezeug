3212ff468fdd0ce13ccb642beecfcb073507472f
daniel.r.kegel <daniel.r.kegel@065a81bf-8143-0410-bb62-c3a588fc7d44>
2010-05-07 04:06:48 +0000

    yagmark-daily: upload as soon as each iteration is done

--- yagmark-daily.sh
+++ yagmark-daily.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Trivial script to build wine and run yagmarkk ten times, uploading as each iteration is done.
 set -e
 set -x
 WINEDIR=$HOME/wine-git
@@ -15,7 +16,9 @@ git pull
 patch -p1 < 3dmark06.patch
 
 ./configure --prefix=/usr/local/wine --without-nas
-make -j3
+ncpus=`awk '/processor/' < /proc/cpuinfo | wc -l`
+njobs=`expr $ncpus + 1`
+make -j$njobs
 cd ~/winezeug
 
 i=0
@@ -23,9 +26,10 @@ while test $i -lt 10
 do
    sh yagmark 3dmark2000 3dmark2001 3dmark06
    i=`expr $i + 1`
+
+   if test "$1" != ""
+   then
+       rsync -a results/* $1
+   fi
 done
 
-if test "$1" != ""
-then
-    rsync -a results/* $1
-fi
