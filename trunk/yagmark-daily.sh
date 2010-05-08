c22770c0861b4be93e8799da196e089121d13d06
daniel.r.kegel <daniel.r.kegel@065a81bf-8143-0410-bb62-c3a588fc7d44>
2010-05-08 00:00:29 +0000

    yagmark-daily.sh: work on windows; add heaven2_d3d9 and heaven2_gl

--- trunk/yagmark-daily.sh
+++ trunk/yagmark-daily.sh
@@ -2,29 +2,37 @@
 # Trivial script to build wine and run yagmarkk ten times, uploading as each iteration is done.
 set -e
 set -x
-WINEDIR=$HOME/wine-git
-WINE=$WINEDIR/wine
-export WINE
-WINEPREFIX=$HOME/.wine-yagmark-daily
-export WINEPREFIX
 
-cd $WINEDIR
-make clean
-git diff > foo.diff
-patch -R -p1 < foo.diff
-git pull
-patch -p1 < 3dmark06.patch
+sync_and_build_wine()
+{
+    WINEDIR=$HOME/wine-git
+    WINE=$WINEDIR/wine
+    export WINE
+    WINEPREFIX=$HOME/.wine-yagmark-daily
+    export WINEPREFIX
 
-./configure --prefix=/usr/local/wine --without-nas
-ncpus=`awk '/processor/' < /proc/cpuinfo | wc -l`
-njobs=`expr $ncpus + 1`
-make -j$njobs
-cd ~/winezeug
+    cd $WINEDIR
+    make clean
+    git diff > foo.diff
+    patch -R -p1 < foo.diff
+    git pull
+    patch -p1 < 3dmark06.patch
+
+    ./configure --prefix=/usr/local/wine --without-nas
+    ncpus=`awk '/processor/' < /proc/cpuinfo | wc -l`
+    njobs=`expr $ncpus + 1`
+    make -j$njobs
+    cd ~/winezeug
+}
+
+case "$OS" in
+"") sync_and_build_wine ;;
+esac
 
 i=0
 while test $i -lt 10
 do
-   sh yagmark 3dmark2000 3dmark2001 3dmark06
+   sh yagmark heaven2_d3d9 heaven2_gl 3dmark2000 3dmark2001 3dmark06
    i=`expr $i + 1`
 
    if test "$1" != ""
