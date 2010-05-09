91a76e11201a6175d1964cb77781a34f9f5e69e1
daniel.r.kegel <daniel.r.kegel@065a81bf-8143-0410-bb62-c3a588fc7d44>
2010-05-09 03:46:25 +0000

    cebit2009: remove.  Didn't really belong in winezeug.

--- cebit2009/commits.cmd
+++
@@ -1,8 +0,0 @@
-set terminal postscript color "Helvetica" 16 portrait
-set output 'commits.eps'
-set style fill solid
-set ylabel "Changes/year"
-set yrange [2000:13000]
-set size square 
-plot "commits.dat" using 2: xtic(1) with histogram \
- title "Wine source code checkins per year"
