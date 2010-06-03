2678753525cb0f40d2e1f36eef172bb6e547fef4
daniel.r.kegel <daniel.r.kegel@065a81bf-8143-0410-bb62-c3a588fc7d44>
2010-06-03 14:18:33 +0000

    Added two scripts used to run yagmark continuously.

---
+++ trunk/yagmark-sync.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+set -e
+set -x
+
+# We grep error messages, so make them all English
+LANG=C
+
+setup()
+{
+   # Unapply local patches before sync
+   git diff > yagmark-sync-$$.patch
+   patch -R -p1 < yagmark-sync-$$.patch
+   cleaned=0
+   trap cleanup 0 INT 
+}
+
+cleanup()
+{
+   # Reapply local patches after sync
+   test $cleaned = 1 && exit
+   patch -p1 < yagmark-sync-$$.patch
+   status=$?
+   cleaned=1
+
+   case $status in
+   0) exit 0 ;;
+   *) echo "Patch failed"; exit 1
+   esac
+}
+
+# Returns success if something was pulled
+checked_git_pull()
+{
+  echo pulling
+  git pull > git.log 2>&1
+  cat git.log
+  if grep -q "Already up-to-date." < git.log
+  then
+    return 1
+  fi
+  return 0
+}
+
+wait_for_update()
+{
+  # Wait until some change
+  git show | head -n 1 > rev1
+  while true
+  do
+    checked_git_pull || true
+    git show | head -n 1 > rev2
+    if grep commit rev1 && grep commit rev2 && ! cmp rev1 rev2
+    then
+     break
+    fi
+    sleep 600
+  done
+
+  # Then wait for no change for ten minutes 
+  sleep 600
+  while checked_git_pull
+  do 
+     sleep 600
+  done
+}
+
+setup
+wait_for_update
+cleanup
