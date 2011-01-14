b3cfb05384ec41e450fc59970ad6750b2ea24cbd
daniel.r.kegel <daniel.r.kegel@gmail.com>
2011-01-14 11:32:43 +0000

    wustats.sh: handle newer format, ignore more synonyms

--- wustats.sh
+++ wustats.sh
@@ -3,14 +3,14 @@
 # Before running, grab the web log manually, place it in usage.log
 
 # See what verbs wisotool2 has today
-grep '^w_metadata' wisotool2 | grep -v '()' | awk '!/games/ {print $2}' | sort > wisotool2.loads
+grep '^w_metadata' winetricks-alpha | grep -v '()' | awk '!/xgames/ {print $2}' | sort > wisotool2.loads
 
 # Find what verbs people are using; discard any duplicate verbs from same person,
 # then show tally of verbs not yet in wisotool2
 cat usage.log |
     cut -d' ' -f1,7 |
     grep winetricks-usage |
-    sed 's,/data/winetricks-usage?20101222-,,;s/%20/ /g' |
+    sed 's,/data/winetricks-usage?[0-9]*-,,;s/%20/ /g' |
     perl -e 'while (<STDIN>) {chomp; @x=split(" "); $ip=shift(@x); foreach (@x) { print "$ip $_\n"; }}' > usage.raw
 
     cat usage.raw |
@@ -21,8 +21,9 @@ cat usage.log |
     sort -nr > usage.txt
 
     cat usage.raw |
+    egrep -v '/data/|winetricks=|Version:|version|dlls' |
     fgrep -w -v -f wisotool2.loads |
-    egrep -w -v "fontsmooth-.*|-q|dotnet20sp2|wsh56|comdlg32.ocx" |
+    egrep -w -v "fontsmooth-.*|-q|-v|dotnet20sp2|wsh56|comdlg32.ocx|vsm-hard|--optin|npm-repack|vcrun2005sp1|psm=on|fm20|glsl-enable|glsl-disable|ie6_full|cc580|xlive|python|vd=[0-9x]*|list-manual-download|list-installed|list-cached|list-download|list|vbrun60|psm=off|mwo=disabled|dotnet1|dotnet2" |
     sort -u | 
     cut -d' ' -f2 |
     sort |
