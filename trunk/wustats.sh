24ada636bcbd8609abbbb865c037756dc695ed42
daniel.r.kegel@gmail.com <daniel.r.kegel@gmail.com@065a81bf-8143-0410-bb62-c3a588fc7d44>
2011-01-14 11:34:15 +0000

    wustats.sh: whoops, don't ignore everything containing dlls

--- trunk/wustats.sh
+++ trunk/wustats.sh
@@ -21,7 +21,8 @@ cat usage.log |
     sort -nr > usage.txt
 
     cat usage.raw |
-    egrep -v '/data/|winetricks=|Version:|version|dlls' |
+    egrep -v '/data/|winetricks=|Version:|version' |
+    egrep -w -v 'dlls' |
     fgrep -w -v -f wisotool2.loads |
     egrep -w -v "fontsmooth-.*|-q|-v|dotnet20sp2|wsh56|comdlg32.ocx|vsm-hard|--optin|npm-repack|vcrun2005sp1|psm=on|fm20|glsl-enable|glsl-disable|ie6_full|cc580|xlive|python|vd=[0-9x]*|list-manual-download|list-installed|list-cached|list-download|list|vbrun60|psm=off|mwo=disabled|dotnet1|dotnet2" |
     sort -u | 
