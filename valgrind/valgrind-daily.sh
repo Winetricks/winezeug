dc7848fb2ea75eb368038d1f340e7ccfc03f815e
austinenglish <austinenglish@gmail.com>
2011-01-18 07:47:19 +0000

    valgrind: cleanup valgrind-daily.sh, add a few more suppressions (nvidia driver, expected crypt32 failures)

--- valgrind/valgrind-daily.sh
+++ valgrind/valgrind-daily.sh
@@ -5,12 +5,15 @@ set -x
 set -e
 
 # Must be run from the wine tree
-WINESRC=`pwd`
+WINESRC="$HOME/wine-git"
 # Prepare for calling winetricks
 export WINEPREFIX=$HOME/.wine-test
 export WINE=$WINESRC/wine
 # Convenience variable
 WINESERVER=$WINESRC/server/wineserver
+
+cd $WINESRC
+
 if test ! -f $WINESRC/configure 
 then
     echo "couldn't find $WINESRC/configure"
@@ -20,9 +23,6 @@ fi
 # We grep error messages, so make them all English
 LANG=C
 
-# Choose the version of valgrind
-PATH=/usr/local/valgrind-10903/bin:$PATH
-
 $WINESERVER -k || true
 rm -rf $WINEPREFIX
 
@@ -31,52 +31,37 @@ mkdir -p logs
 # Build a fresh wine, if desired
 if true
 then
-    ./configure CFLAGS="-g -O0 -fno-inline" --prefix=/usr/local/wine
+    ./configure CFLAGS="-g -O0 -fno-inline"
     make clean
     time make -j4
 fi
 make testclean
 
 # Load gecko and disable the crash dialog
-sh winetricks gecko nocrashdialog heapcheck || true
+if test ! -f winetricks
+then
+    wget http://winezeug.googlecode.com/svn/trunk/winetricks
+fi
 
-# keep a notepad up to avoid repeated startup penalty
-# even though that hides some errors
-$WINESERVER -w
-$WINE notepad &
+sh winetricks nocrashdialog heapcheck || true
+
+# start a minimized winemine to avoid repeated startup penalty even though that hides some errors
+# Note: running `wineserver -p` is not enough, because that only runs wineserver, not any services
+$WINE start /M winemine
 
 # Should we use date or id from git log?
 DATE=`date +%F-%H.%M`
 # Get info about what tree we're testing
 git log -n 1 > logs/$DATE.log
 
-# Disable any tests known to crash reliably and noisily
-# http://bugs.winehq.org/show_bug.cgi?id=20917
-touch dlls/d3d8/tests/device.ok
-touch dlls/d3d9/tests/device.ok
-touch dlls/ddrawex/tests/surface.ok
-# http://bugs.winehq.org/show_bug.cgi?id=20925
-touch dlls/d3d8/tests/visual.ok
-touch dlls/d3d9/tests/visual.ok
-touch dlls/d3dx9_36/tests/core.ok
-
-# Disable any tests known to hang reliably
-# http://bugs.winehq.org/show_bug.cgi?id=20919
-touch dlls/kernel32/tests/process.ok
-touch dlls/ole32/tests/marshal.ok
-touch dlls/shdocvw/tests/webbrowser.ok
-touch dlls/urlmon/tests/url.ok
-touch dlls/winmm/tests/mci.ok
-touch dlls/winmm/tests/wave.ok
-
-# Finally run the test
-export VALGRIND_OPTS="-q --trace-children=yes --track-origins=yes --gen-suppressions=all --suppressions=$PWD/tools/valgrind/valgrind-suppressions --leak-check=full --num-callers=20  --workaround-gcc296-bugs=yes --show-possible=no"
+# Finally run the tests
+export VALGRIND_OPTS="-q --trace-children=yes --track-origins=yes --gen-suppressions=all --suppressions=$WINESRC/tools/valgrind/valgrind-suppressions --leak-check=full --num-callers=20  --workaround-gcc296-bugs=yes --vex-iropt-precise-memory-exns=yes"
 export WINETEST_TIMEOUT=600
 export WINETEST_WRAPPER=valgrind
 export WINE_HEAP_TAIL_REDZONE=32
-time make -k -j2 test >> logs/$DATE.log 2>&1 || true
+time make -k test >> logs/$DATE.log 2>&1 || true
 
-# Kill off our notepad and any stragglers
+# Kill off winemine and any stragglers
 $WINESERVER -k || true
 
 # Analyze the log
