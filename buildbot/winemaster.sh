e0f22d578bad8baea33578d1b97d2f879f99cb75
daniel.r.kegel <daniel.r.kegel@065a81bf-8143-0410-bb62-c3a588fc7d44>
2011-08-18 03:03:51 +0000

    Split winebuildbot.sh into wineslave.sh and winemaster.sh

---
+++ buildbot/winemaster.sh
@@ -0,0 +1,185 @@
+#!/bin/sh
+# Start a wine buildbot master
+# Copyright 2011, Dan Kegel
+# LGPL
+
+set -x
+set -e
+
+TOP=$HOME/winemaster.dir
+
+# Get email address of user from environment for use by the 'try' command.
+# If not set, default to username@hostname.
+EMAIL=${EMAIL:-$LOGNAME@`hostname`}
+
+#----- Helper functions -----------------------------------------------------
+
+usage() {
+    set +x
+    echo "Usage: $0 command ..."
+    echo "Commands:"
+    echo "   destroy"
+    echo "   install_prereqs"
+    echo "   create"
+    echo "   start"
+    echo "   tail"
+    echo "   demo"
+    echo "   patchwatcher"
+
+    exit 1
+}
+
+#----- Functions invoked by user when setting up or starting master -----------
+
+install_prereqs() {
+    # For Ubuntu.  Other systems may differ.
+    # Needed for buildbot
+    sudo apt-get install python-dev python-virtualenv 
+}
+
+destroy() {
+    if test -d $TOP
+    then
+        stop_master || true
+        rm -rf $TOP || true
+    fi
+}
+
+create_master() {
+    (
+    # Master
+    mkdir -p $TOP
+    cd $TOP
+    virtualenv --no-site-packages sandbox
+    cd $TOP/sandbox
+    . bin/activate
+    if false
+    then
+        easy_install buildbot
+    elif false
+    then
+        # Here's how to install from a source tarball
+        wget -c http://buildbot.googlecode.com/files/buildbot-0.8.4p2.tar.gz
+        tar -xzvf buildbot-0.8.4p2.tar.gz
+        cd buildbot-0.8.4p2
+        python setup.py install
+        cd ..
+    else
+        # Here's how to install master from trunk
+        # (Needed until buildbot-0.8.5 is released,
+        # since it has fixes for the try server / mail notifier.)
+        # BTW rerunning 'pip install -emaster' takes less than a second, 
+        # and seems to be how buildbot developers test their code
+        test -d buildbot-git || git clone git://github.com/buildbot/buildbot.git buildbot-git
+        cd buildbot-git
+        export PIP_USE_MIRRORS=true
+        pip install -emaster
+        cd ..
+    fi
+    buildbot create-master master
+    )
+}
+
+start_master() {
+    (
+    cd $TOP/sandbox
+    . bin/activate
+    cp $SRC/master.cfg $VIRTUAL_ENV/master
+    buildbot start $VIRTUAL_ENV/master
+    )
+}
+
+stop_master() {
+    (
+    cd $TOP/sandbox
+    . bin/activate
+    buildbot stop $VIRTUAL_ENV/master
+    )
+}
+
+# Shows how to bring up a demo master
+demo() {
+    destroy
+    install_prereqs
+    create_master
+    start_master
+}
+
+#--------- Functions used to implement patchwatcher ---------
+
+do_try() {
+    (
+    if test "$1" = ""
+    then
+        echo "need patch name"
+        exit 1
+    fi
+    who=$2
+    # The --properties flag uses comma as a delimiter, so we have to change all commas in comments to something else
+    subject="`echo $3 | tr ',' ';'`"
+    cd $TOP/sandbox
+    . bin/activate
+    # FIXME: import username and password from another file so
+    # it doesn't show up in svn.  Must match those in master.cfg.
+    # FIXME: Use real hostname for master.
+    # Always use -p 1 for wine patches, since that's the project's convention.
+    buildbot try $wait --who $who --properties=comment="$subject" --connect=pb --master=127.0.0.1:5555 --username=fred --passwd=trybot --diff=$1 -p 1
+    )
+}
+
+# Try the most recent not-yet-done patch series from source.winehq.org/patches
+do_pulltry() {
+    series=`perl parsepatch.pl 1`
+    if test "$series"
+    then
+        # For now, just concatenate series together into one big patch
+        rm -f series_*.patch
+        for id in $series
+        do
+            wget -O series_$id.patch http://source.winehq.org/patches/data/$id
+        done
+        cat series_*.patch > series.patch
+        author_email=`grep '^From:' < series.patch | head -n 1 | sed 's/^From: //;s/.*<//;s/>.*//'`
+        subject="`grep '^Subject:' < series.patch | uniq`"
+        do_try `pwd`/series.patch $author_email "${id}: ${subject}"
+    fi
+}
+
+# Sit and test new patches forever
+do_patchwatcher() {
+    while true
+    do
+       do_pulltry || true
+       # Launch at most one patch per minute
+       sleep 60
+    done
+}
+
+#--------- Main program ---------------------------------------------------------
+
+SRC=`dirname $0`
+SRC=`cd $SRC; pwd`
+
+if ! test "$1"
+then
+    usage
+fi
+
+while test "$1"
+do
+    arg="$1"
+    shift
+    case "$arg" in
+    destroy) destroy;;
+    install_prereqs) install_prereqs;;
+    create_master) create_master;;
+    start) start_master;;
+    stop) stop_master;;
+    tail) tail -f $TOP/sandbox/master/twistd.log;;
+    demo) demo;;
+    pulltry) do_pulltry;;
+    pw|patchwatcher) do_patchwatcher;;
+    try) do_try $1 $EMAIL; shift;;
+    *) usage ;;
+    esac
+done
---
+++ buildbot/wineslave.sh
@@ -0,0 +1,247 @@
+#!/bin/sh
+# Start a wine buildbot slave
+# Copyright 2011, Dan Kegel
+# LGPL
+
+set -e
+
+TOP=$HOME/wineslave.dir
+
+#----- Helper functions -----------------------------------------------------
+
+usage() {
+    echo "Usage: $0 command ..."
+    echo "Commands:"
+    echo "   destroy"
+    echo "   install_prereqs"
+    echo "   create BUILDMASTER:PORT USERNAME PASSWORD"
+    echo "   start"
+    echo "   tail"
+    echo "   patch"
+    echo "   configure"
+    echo "   build"
+    echo "   test"
+    echo "   demo"
+
+    system_info
+    exit 1
+}
+
+system_osname() {
+    # FIXME: do this portably
+    lsb_release -d -s
+}
+
+system_cpuname() {
+    # FIXME: do this portably
+    cat /proc/cpuinfo  | grep "model name" | uniq | sed 's/model name.: //'
+}
+
+system_numcpus() {
+    if test "$NUMBER_OF_PROCESSORS"
+    then
+        echo $NUMBER_OF_PROCESSORS
+    elif sysctl -n hw.ncpu 2> /dev/null
+    then
+        # Mac, freebsd
+        :
+    else
+        # x86 linux
+        grep '^processor' /proc/cpuinfo | wc -l
+    fi
+}
+
+system_ram_megabytes() {
+    # FIXME: do this portably
+    free | awk '/Mem:/ {printf("%d\n", $2 / 1024); }'
+}
+
+system_gpu() {
+    # FIXME: do this portably
+    glxinfo | egrep "OpenGL renderer string:|OpenGL version string" | sed 's/.*string: //'
+}
+
+system_info() {
+    echo "os:    " `system_osname`
+    echo "ram:   " `system_ram_megabytes` MB
+    echo "cpu:   " `system_cpuname`
+    echo "gpu:   " `system_gpu`
+}
+
+#----- Functions invoked by user when setting up or starting slave -----------
+
+install_prereqs() {
+    # For Ubuntu.  Other systems may differ.
+    # Needed for buildbot
+    sudo apt-get install python-dev python-virtualenv 
+    # Needed to apply patches
+    sudo apt-get install autoconf
+    # Needed to work around http://bugs.winehq.org/show_bug.cgi?id=28097
+    sudo apt-get install ttf-mscorefonts-installer
+    # Needed to pass rpcrt4 tests
+    sudo apt-get install winbind
+}
+
+destroy() {
+    if test -d $TOP
+    then
+        stop_slave || true
+        rm -rf $TOP || true
+    fi
+}
+
+# Create a slave and say how to connect to the build master
+# Usage: create_slave buildmaster:port slavename slavepassword
+create_slave() {
+    (
+    if test "$1" = ""
+    then
+        echo "need buildhost:port"
+        exit 1
+    fi
+    if test "$2" = ""
+    then
+        echo "need slave name"
+        exit 1
+    fi
+    if test "$3" = ""
+    then
+        echo "need slave password"
+        exit 1
+    fi
+    mkdir -p $TOP
+    cd $TOP
+    test -d sandbox || virtualenv --no-site-packages sandbox
+    cd $TOP/sandbox
+    . bin/activate
+    easy_install buildbot-slave
+    buildslave create-slave slave $1 $2 $3
+    cp $SRC/wineslave.sh $TOP/sandbox/bin/wineslave.sh
+    chmod +x $TOP/sandbox/bin/wineslave.sh
+    cp $SRC/*-ignore-*.patch $TOP/sandbox/bin
+    )
+
+    echo "Filling in $TOP/sandbox/slave/info/host with following info:"
+    system_info
+    system_info > $TOP/sandbox/slave/info/host
+
+    echo "Please put your name and email address in $TOP/sandbox/slave/info/admin !"
+}
+
+start_slave() {
+    (
+    cd $TOP/sandbox
+    . bin/activate
+    buildslave start $VIRTUAL_ENV/slave
+    )
+}
+
+stop_slave() {
+    (
+    cd $TOP/sandbox
+    . bin/activate
+    buildslave stop $VIRTUAL_ENV/slave
+    )
+}
+
+# Shows how to bring up a demo slave that connects to a local buildbot.
+# A real slave would connect to, say, buildbot.winehq.org instead of localhost.
+demo() {
+    destroy
+    install_prereqs
+    create_slave localhost:9989 example-slave pass
+    start_slave
+}
+
+#--------- Functions used while slave is running; invoked by master.cfg ---------
+
+# Apply patches needed to skip flaky tests
+do_patch() {
+    cd $TOP/sandbox/slave/runtests/build
+    for p in $SRC/*-ignore-*.patch
+    do
+        echo $p
+        patch -p1 < $p
+    done
+}
+
+do_configure() {
+    cd $TOP/sandbox/slave/runtests/build
+    # Running make_makefiles without adding new source files to git
+    # generates invalid Makefile.in's; let's hope each patch that adds a new
+    # source file contains the needed Makefile.in changes.
+    #tools/make_makefiles
+    autoconf
+    CFLAGS="-g -O0" ./configure
+}
+
+do_build() {
+    cd $TOP/sandbox/slave/runtests/build
+    make -j`system_numcpus`
+}
+
+do_test() {
+    cd $TOP/sandbox/slave/runtests/build
+    rm -rf wineprefix
+    export WINEPREFIX=`pwd`/wineprefix
+    # Many tests only work in english locale
+    LANG=en_US.UTF-8
+    # winetricks vd=800x600
+    ./wine reg add HKCU\\Software\\Wine\\Explorer /v Desktop /d Default
+    ./wine reg add HKCU\\Software\\Wine\\Explorer\\Desktops /v Default /d 800x600
+    # winetricks nocrashdialog
+    ./wine reg add HKCU\\Software\\Wine\\WineDbg /v ShowCrashDialog /t REG_DWORD /d 0
+    # Blacklist some tests
+    # http://bugs.winehq.org/show_bug.cgi?id=12053
+    touch dlls/user32/tests/msg.ok
+    touch dlls/user32/tests/win.ok
+    touch dlls/user32/tests/input.ok
+    # http://bugs.winehq.org/show_bug.cgi?id=28038
+    touch dlls/wininet/tests/urlcache.ok
+    # http://bugs.winehq.org/show_bug.cgi?id=28109
+    touch dlls/winmm/tests/capture.ok
+    # http://bugs.winehq.org/show_bug.cgi?id=28048
+    touch dlls/winmm/tests/wave.ok
+    # Blacklist until http://www.winehq.org/pipermail/wine-patches/2011-August/105358.html in
+    touch dlls/winhttp/tests/winhttp.ok
+    # http://bugs.winehq.org/show_bug.cgi?id=28102
+    touch dlls/ws2_32/tests/sock.ok
+    # http://bugs.winehq.org/show_bug.cgi?id=28108
+    touch dlls/urlmon/tests/url.ok
+    # Avoid race condition with registry that caused some tests to not run
+    # in a virtual desktop?
+    server/wineserver -w
+    # Get elapsed time of each test
+    WINETEST_WRAPPER=time make -k test
+}
+
+#--------- Main program ---------------------------------------------------------
+
+SRC=`dirname $0`
+SRC=`cd $SRC; pwd`
+
+if ! test "$1"
+then
+    usage
+fi
+
+while test "$1"
+do
+    arg="$1"
+    shift
+    case "$arg" in
+    info) system_info;;
+    install_prereqs) install_prereqs;;
+    destroy) destroy;;
+    create) create_slave;;
+    start) start_slave;;
+    stop) stop_slave;;
+    tail) tail -f $TOP/sandbox/slave/twistd.log;;
+    patch) do_patch;;
+    configure) do_configure;;
+    build) do_build;;
+    test) do_test;;
+    demo) demo;;
+    *) usage;;
+    esac
+done
