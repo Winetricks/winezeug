d1e8b67bfe9aaa96d68389a4242457e10d5f695c
daniel.r.kegel <daniel.r.kegel@065a81bf-8143-0410-bb62-c3a588fc7d44>
2011-09-01 03:21:51 +0000

    Since gcc-2.95.3 is no longer packaged for ubuntu, here's how to build it from source.

---
+++ trunk/buildbot/build-gcc-2.95.3.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Script to build gcc-2.95.3 for use with wine
+set -e
+set -x
+
+if ! test -d /usr/include/asm
+then
+    sudo ln -s /usr/include/i386-linux-gnu/asm /usr/include/asm
+fi
+
+cat > gcc-2.95.3-collect2.patch <<_EOF_
+--- gcc-2.95.3/gcc/collect2.c.old	2011-08-31 19:36:54.000000000 -0700
++++ gcc-2.95.3/gcc/collect2.c	2011-08-31 19:37:27.000000000 -0700
+@@ -1759,7 +1759,7 @@
+   if (redir)
+     {
+       /* Open response file.  */
+-      redir_handle = open (redir, O_WRONLY | O_TRUNC | O_CREAT);
++      redir_handle = open (redir, O_WRONLY | O_TRUNC | O_CREAT, 0666);
+ 
+       /* Duplicate the stdout and stderr file handles
+ 	 so they can be restored later.  */
+_EOF_
+
+wget -c http://ftp.gnu.org/pub/gnu/gcc/gcc-2.95.3/gcc-core-2.95.3.tar.gz
+tar -xzvf gcc-core-2.95.3.tar.gz 
+patch -p0 < gcc-2.95.3-collect2.patch
+cd gcc-2.95.3/
+./configure --enable-languages=c --prefix=/usr/local/gcc-2.95.3
+make -j3
+#sudo make install
