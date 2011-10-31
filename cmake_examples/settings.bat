1b4c6f3d5fc0264a1c58b4fd55afdb42db3ab905
daniel.r.kegel <daniel.r.kegel@065a81bf-8143-0410-bb62-c3a588fc7d44>
2011-10-31 01:12:42 +0000

    cmake_examples: set BOOST_ALL_NO_LIB to fix link failure, add missing path for rc.exe, remove mistaken old link failure workaround

--- cmake_examples/settings.bat
+++ cmake_examples/settings.bat
@@ -1,14 +1,23 @@
 REM adjust this to match your system
 
+REM You can get the compiler from several places.
+REM Uncomment the one you use.  There are also compilers
+REM in the Windows 7 Platform SDK, which you need anyway
+REM for Windows .lib files, in case you don't need an IDE.
 REM Visual C++ 2005 Express 
 rem call "c:\Program Files\Microsoft Visual Studio 8\Common7\Tools\vsvars32.bat"
+
 REM Windows 7 Platform SDK
 call "c:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
 set LIB=c:/Program Files/Microsoft SDKs/Windows/v7.0/Lib;%LIB%
+REM For some reason, vcvars32 doesn't completely set PATH
+PATH %PATH%;C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin
 
-rem Boosty things
+REM Third party libraries and tools
+
+REM Tell CMake where Boost lives.
 set BOOST_ROOT=c:/Program Files/boost/boost_1_47
-rem set LIB=C:/Program Files/boost/boost_1_47/lib;%LIB%
+REM If you're using the Boost DLLs, put them on the PATH.
 PATH C:\Program Files\boost\boost_1_47\lib;%PATH%
 
 rem CMake (if it didn't already put itself on the path)
