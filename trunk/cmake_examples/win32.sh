176eefe10d012e9a2aad020ea62d4998e946f560
daniel.r.kegel <daniel.r.kegel@065a81bf-8143-0410-bb62-c3a588fc7d44>
2011-11-02 19:10:16 +0000

    Back off to boost-1.46.1 because cmake-2.8.6 can't find boost-1.47 (sometimes?)

--- trunk/cmake_examples/win32.sh
+++ trunk/cmake_examples/win32.sh
@@ -19,13 +19,15 @@ run() {
 test -f cmake-2.8.6-win32-x86.exe || wget http://www.cmake.org/files/v2.8/cmake-2.8.6-win32-x86.exe
 run cmake-2.8.6-win32-x86.exe /S
 
-test -f boost_1_47_setup.exe || wget http://boostpro.com/download/boost_1_47_setup.exe
-run boost_1_47_setup.exe 
+# Boost 1.47 is available, but cmake-2.8.6's findBoost doesn't support it yet
+test -f boost_1_46_1_setup.exe || wget http://boostpro.com/download/boost_1_46_1_setup.exe
+run boost_1_46_1_setup.exe 
 
 test -f winetricks || wget http://winetricks.googlecode.com/svn/trunk/src/winetricks
 # Do as silent an install as winetricks knows how to do
 # In the Platform SDK dialog, choose just the windows headers, libraries, and compiler
-# sh winetricks --no-isolate -q psdkwin7 
 
-# Some examples might require Visual C++ later
-# sh winetricks vc2005express
+# Install either psdkwin7 and vc2005express:
+# sh winetricks --no-isolate -q psdkwin7 vc2005express
+# Or vc2008express:
+# http://www.microsoft.com/visualstudio/en-us/products/2008-editions/express
