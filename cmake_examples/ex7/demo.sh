5501cb8cb01e0b3ed1286f7d06d80b7581ae0650
daniel.r.kegel <daniel.r.kegel@065a81bf-8143-0410-bb62-c3a588fc7d44>
2011-11-11 19:24:38 +0000

    Fix svn support for multiple source directories

--- cmake_examples/ex7/demo.sh
+++ cmake_examples/ex7/demo.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 set -e
 set -x
+
+SRC=`dirname $0`
+SRC=`cd $SRC; pwd`
+
 mkdir build
 cd build
+
+# Generate null projects for each source directory by hand
+# rather than asking the cdt generator to do it, since 
+# it can't handle more than one
+for dir in demo libsrc
+do
+    sed "s/PROJNAME/_$dir/" < ../skeleton.project > ../$dir/.project
+done
+
 cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug ../demo
-echo 'Now start up eclipse and do'
-echo 'File / Import / General / Existing Projects Into Workspace, browse to '`pwd`', and click OK'
-echo 'Also go to Window -> Preferences -> General -> Workspace'
+
+echo "File / Import / General / Existing PROJECTS Into Workspace, then browse to"
+echo "and select $SRC (the parent directory)."
+echo "You should see projects _demo and _libsrc, and the Team menu on each"
+echo "should show that it's connected to svn."
+echo ""
+echo "Also, if you haven't yet, go to Window -> Preferences -> General -> Workspace"
 echo 'and tick both "Refresh using native hooks or polling" and "Refresh on access"'
 echo 'so Eclipse will notice when cmake updates the project (when, say, you add a source file in CMakeLists.txt)"'
 #make
