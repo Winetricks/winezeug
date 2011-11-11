bc0022ae0d79d30f8a359de79ac080ab2c621591
daniel.r.kegel <daniel.r.kegel@065a81bf-8143-0410-bb62-c3a588fc7d44>
2011-11-11 17:19:28 +0000

    ex6: document how to set up subclipse and import both projects at once

--- trunk/cmake_examples/ex6/demo.sh
+++ trunk/cmake_examples/ex6/demo.sh
@@ -3,9 +3,9 @@ set -e
 set -x
 mkdir build
 cd build
-cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug ../demo
+cmake -G"Eclipse CDT4 - Unix Makefiles" -DECLIPSE_CDT4_GENERATE_SOURCE_PROJECT=TRUE -DCMAKE_BUILD_TYPE=Debug ../demo
 echo 'Now start up eclipse and do'
-echo 'File / Import / General / Existing Projects Into Workspace, browse to '`pwd`', and click OK'
+echo 'File / Import / General / Existing Projects Into Workspace, browse to cmake_examples/ex6, and click OK'
 echo 'Also go to Window -> Preferences -> General -> Workspace'
 echo 'and tick both "Refresh using native hooks or polling" and "Refresh on access"'
 echo 'so Eclipse will notice when cmake updates the project (when, say, you add a source file in CMakeLists.txt)"'
